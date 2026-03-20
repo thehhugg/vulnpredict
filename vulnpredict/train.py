ada5a9058ba5ebc88bc99fbdf90d8635b85f6e12
H <heathermariehuggins@gmail.com>
2026-03-20 11:22:10 -0400

    style: reformat train.py with black and isort

--- vulnpredict/train.py
+++ vulnpredict/train.py
@@ -1,19 +1,23 @@
-import pandas as pd
-from .ml import train_model
 import click
+import pandas as pd
+
+from .ml import train_model
+
 
 @click.command()
-@click.argument('csv_file')
+@click.argument("csv_file")
 def main(csv_file):
     """
     Train the VulnPredict model from a labeled CSV file.
     """
     df = pd.read_csv(csv_file)
-    raw_features = df.drop(columns=['label'])
-    labels = df['label']
+    raw_features = df.drop(columns=["label"])
+    labels = df["label"]
     from .ml import extract_features
-    features = extract_features(raw_features.to_dict(orient='records'))
+
+    features = extract_features(raw_features.to_dict(orient="records"))
     train_model(features, labels)
 
-if __name__ == '__main__':
-    main() 
+
+if __name__ == "__main__":
+    main()
