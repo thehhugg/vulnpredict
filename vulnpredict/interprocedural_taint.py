b186c66b2bf558d992f2787d2c09a67bd9675206
H <heathermariehuggins@gmail.com>
2026-03-20 11:22:03 -0400

    fix: remove unused defaultdict import

--- vulnpredict/interprocedural_taint.py
+++ vulnpredict/interprocedural_taint.py
@@ -1,9 +1,35 @@
 import ast
 import os
-from collections import defaultdict, deque
+from collections import deque
 
-DANGEROUS_FUNCTIONS = {'eval', 'exec', 'compile', 'execfile', 'input', 'os.system', 'subprocess.Popen', 'subprocess.call', 'cursor.execute', 'execute', 'os.popen', 'os.popen2', 'os.popen3', 'os.popen4'}
-TAINT_SOURCES = {'input', 'os.environ', 'sys.argv', 'request.args', 'request.form', 'request.get_json', 'open', 'read', 'recv'}
+DANGEROUS_FUNCTIONS = {
+    "eval",
+    "exec",
+    "compile",
+    "execfile",
+    "input",
+    "os.system",
+    "subprocess.Popen",
+    "subprocess.call",
+    "cursor.execute",
+    "execute",
+    "os.popen",
+    "os.popen2",
+    "os.popen3",
+    "os.popen4",
+}
+TAINT_SOURCES = {
+    "input",
+    "os.environ",
+    "sys.argv",
+    "request.args",
+    "request.form",
+    "request.get_json",
+    "open",
+    "read",
+    "recv",
+}
+
 
 class FunctionInfo:
     def __init__(self, name, args, node, calls, filename, lineno):
@@ -13,6 +39,7 @@ class FunctionInfo:
         self.calls = calls  # list of (callee_name, arg_map, call_lineno)
         self.filename = filename
         self.lineno = lineno
+
 
 class CallVisitor(ast.NodeVisitor):
     def __init__(self, func_args):
@@ -34,7 +61,8 @@ class CallVisitor(ast.NodeVisitor):
             return node.id
         elif isinstance(node, ast.Attribute):
             return f"{self._get_func_name(node.value)}.{node.attr}"
-        return ''
+        return ""
+
 
 def index_functions(path):
     """
@@ -44,9 +72,9 @@ def index_functions(path):
     func_index = {}
     for root, _, files in os.walk(path):
         for file in files:
-            if file.endswith('.py'):
+            if file.endswith(".py"):
                 fpath = os.path.join(root, file)
-                with open(fpath, 'r') as f:
+                with open(fpath, "r") as f:
                     source = f.read()
                 try:
                     tree = ast.parse(source, filename=fpath)
@@ -64,9 +92,10 @@ def index_functions(path):
                             node=node,
                             calls=call_visitor.calls,
                             filename=fpath,
-                            lineno=node.lineno
+                            lineno=node.lineno,
                         )
     return func_index
+
 
 def find_taint_sources(node):
     sources = set()
@@ -79,6 +108,7 @@ def find_taint_sources(node):
                         sources.add(target.id)
     return sources
 
+
 def find_sinks(node):
     sinks = []
     for child in ast.walk(node):
@@ -87,6 +117,7 @@ def find_sinks(node):
             if func_name in DANGEROUS_FUNCTIONS:
                 sinks.append((func_name, child.lineno, child))
     return sinks
+
 
 def analyze_project(path, max_depth=10):
     """
@@ -112,18 +143,20 @@ def analyze_project(path, max_depth=10):
             curr_info = func_index[curr_func]
             # Check for sinks
             for sink_name, sink_lineno, call_node in find_sinks(curr_info.node):
-                for arg in getattr(call_node, 'args', []):
+                for arg in getattr(call_node, "args", []):
                     if isinstance(arg, ast.Name) and arg.id in tainted:
-                        findings.append({
-                            'type': 'interprocedural_taint',
-                            'source_func': chain[0],
-                            'sink_func': curr_func,
-                            'sink': sink_name,
-                            'sink_line': sink_lineno,
-                            'call_chain': list(chain),
-                            'tainted_var': arg.id,
-                            'var_trace': [set(t) for t in trace],
-                        })
+                        findings.append(
+                            {
+                                "type": "interprocedural_taint",
+                                "source_func": chain[0],
+                                "sink_func": curr_func,
+                                "sink": sink_name,
+                                "sink_line": sink_lineno,
+                                "call_chain": list(chain),
+                                "tainted_var": arg.id,
+                                "var_trace": [set(t) for t in trace],
+                            }
+                        )
             # Propagate taint through calls
             for callee, arg_map, call_lineno in curr_info.calls:
                 if callee in func_index:
@@ -133,11 +166,5 @@ def analyze_project(path, max_depth=10):
                         if var in tainted:
                             tainted_params.add(param)
                     if tainted_params:
-                        worklist.append((
-                            callee,
-                            tainted_params,
-                            chain + [callee],
-                            trace + [tainted_params],
-                            depth + 1
-                        ))
-    return findings 
+                        worklist.append((callee, tainted_params, chain + [callee], trace + [tainted_params], depth + 1))
+    return findings
